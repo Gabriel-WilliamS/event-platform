ce65e85ba67ac208c5f6d8379e4c024caecc5187
gabriel-williams <gabrielwilliamcps@gmail.com>
2022-06-25 15:02:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+import { client } from "./lib/apollo";
+import { Event } from "./pages/Event";
+
+function App() {
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router></Router>
+        </BrowserRouter>
+      </ApolloProvider>
+    </div>
+  );
+}
+
+export default App;
